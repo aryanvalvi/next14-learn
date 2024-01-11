6914bf40f74219d11dd6302521c3624f1ae174f4
Aryan <aryanvalvi323@gmail.com>
2024-01-11 08:57:53 +0530

    abc

---
+++ src/components/links/Links.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import styles from "./links.module.css";
+
+const Links = () => {
+  const links = [
+    {
+      title: "Homepage",
+      path: "/",
+    },
+    {
+      title: "About",
+      path: "/about",
+    },
+    {
+      title: "Contact",
+      path: "/contact",
+    },
+    {
+      title: "Blog",
+      path: "/blog",
+    },
+  ];
+
+  return (
+    <div className={styles.links}>
+      {links.map((link) => {
+        return (
+          <>
+            <Link href={link.path} key={link.path}>
+              {link.title}
+            </Link>
+          </>
+        );
+      })}
+    </div>
+  );
+};
+export default Links;
---
+++ src/components/navLink/navLink.jsx
@@ -0,0 +1,5 @@
+import styles from "./navLink.module.css";
+const NavLink = () => {
+  return <div>NavLink</div>;
+};
+export default NavLink;
